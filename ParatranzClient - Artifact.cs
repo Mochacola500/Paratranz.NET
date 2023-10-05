790a6047cc9cf422fc377bc2215892d1be70c101
drakpink <54413553+drakpink@users.noreply.github.com>
2023-10-06 01:49:51 +0900

    Implement Artifact

--- ParatranzClient - Artifact.cs
+++ ParatranzClient - Artifact.cs
@@ -11,24 +11,18 @@ namespace ParatranzAPI
             return GetAsync<ParatranzArtifact>(url, token);
         }
 
+        public Task<ParatranzBuildInfo?> BuildArtifactAsync(int projectId, CancellationToken token = default)
+        {
+            var url = "projects".AppendPathSegments(projectId, "artifacts");
+
+            return PostAsync<object, ParatranzBuildInfo>(url, null, token);
+        }
+
         public Task<Stream?> DownloadArtifactAsync(int projectId, CancellationToken token = default)
         {
             var url = "projects".AppendPathSegments(projectId, "artifacts", "download");
 
             return DownloadAsync(url, token);
         }
-    }
-
-    public class ParatranzArtifact
-    {
-        public int id { get; set; }
-        public string? createdAt { get; set; }
-        public int project { get; set; }
-        public int total { get; set; }
-        public int translated { get; set; }
-        public int disputed { get; set; }
-        public int reviewed { get; set; }
-        public int hidden { get; set; }
-        public int duration { get; set; }
     }
 }
---
+++ Schema/ParatranzArtifact.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ParatranzAPI
+{
+    public record ParatranzArtifact
+    {
+        public int Id { get; init; }
+        public DateTime? CreatedAt { get; init; }
+        public int Project { get; init; }
+        public int Total { get; init; }
+        public int Translated { get; init; }
+        public int Disputed { get; init; }
+        public int Reviewed { get; init; }
+        public int Hidden { get; init; }
+        public int Duration { get; init; }
+    }
+
+    public record ParatranzBuildInfo
+    {
+        public int Project { get; init; }
+        //public string Type { get; init; }
+        public int uid { get; init; }
+        public int Id { get; init; }
+        public DateTime? CreatedAt { get; init; }
+        //public DateTime? StartedAt { get; init; }
+        //public DateTime? FinishedAt { get; init; }
+        //public DateTime? ScheduledAt { get; init; }
+    }
+}
