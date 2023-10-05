68bfde15474c05069019e9bea6639b3a2e67e833
drakpink <54413553+drakpink@users.noreply.github.com>
2023-10-06 01:29:49 +0900

    Implement Score

--- ParatranzClient - Score.cs
+++ ParatranzClient - Score.cs
@@ -4,7 +4,7 @@ namespace ParatranzAPI
 {
     public partial class ParatranzClient : IDisposable
     {
-        public Task<ParatranzScorePage?> GetScorePageAsync(int projectId, int uid, OperationType type, string start, string end, int page = 1, int pageSize = 50, CancellationToken token = default)
+        public Task<ParatranzScorePage?> GetScorePageAsync(int projectId, int uid, OperationType type, DateTime? start, DateTime? end, int page = 1, int pageSize = 50, CancellationToken token = default)
         {
             var type_str = type.ToString();
             var query = new
@@ -22,33 +22,5 @@ namespace ParatranzAPI
 
             return GetAsync<ParatranzScorePage>(url, token);
         }
-    }
-
-    public enum OperationType
-    {
-        translate,
-        edit,
-        review
-    }
-
-    public class ParatranzScorePage
-    {
-        public int page { get; set; }
-        public int pageSize { get; set; }
-        public int rowCount { get; set; }
-        public int pageCount { get; set; }
-        public string? createdAt { get; set; }
-        public ParatranzScore[]? results { get; set; }
-    }
-
-    public class ParatranzScore
-    {
-        public int Id { get; set; }
-        public string? createdAt { get; set; }
-        public int project { get; set; }
-        public int uid { get; set; }
-        public float @base { get; set; }
-        public float multiplier { get; set; }
-        public float value { get; set; }
     }
 }
---
+++ Schema/ParatranzScore.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ParatranzAPI
+{
+    public enum OperationType
+    {
+        translate,
+        edit,
+        review
+    }
+
+    public record ParatranzScorePage
+    {
+        public int Page { get; init; }
+        public int PageSize { get; init; }
+        public int RowCount { get; init; }
+        public int PageCount { get; init; }
+        public ParatranzScore[]? results { get; init; }
+    }
+
+    public record ParatranzScore
+    {
+        public int Id { get; init; }
+        public DateTime? CreatedAt { get; init; }
+        public int uid { get; init; }
+        public int Project { get; init; }
+        public int tid { get; init; }
+        public float Base { get; init; }
+        public float Multiplier { get; init; }
+        public float Value { get; init; }
+        public ParatranzUser? User { get; init; }
+    }
+}
