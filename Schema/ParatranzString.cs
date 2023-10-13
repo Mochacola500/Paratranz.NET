18f3b45377fae4c5512b80706e0843799bf66389
drakpink <54413553+drakpink@users.noreply.github.com>
2023-10-13 23:07:53 +0900

    Update fileinfo

--- Schema/ParatranzString.cs
+++ Schema/ParatranzString.cs
@@ -11,13 +11,20 @@ namespace ParatranzAPI
 
     public record ParatranzString
     {
+        public record FileInfo
+        {
+            public int Id { get; init; }
+            public string? Name { get; init; }
+            public int Project { set; get; }
+        }
+
         public int Id { get; init; }
         public DateTime? CreatedAt { get; init; }
         public DateTime? UpdatedAt { get; init; }
         public string? Key { get; init; }
         public string? Original { get; init; }
         public string? Translation { get; init; }
-        public int File { get; init; }
+        public FileInfo? File { get; init; }
         public int Stage { get; init; }
         public int Project { get; init; }
         public int uid { get; init; }
