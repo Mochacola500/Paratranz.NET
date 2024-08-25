26493712c19cbc0d5c4c7b76cccc4f560eb714da
Mochacola <techboydonghyeon@gmail.com>
2024-08-26 02:59:26 +0900

    P-4 AuthorizationTest 추가 - 환경변수를 통해 ApiToken을 받아와 인증 검사

---
+++ Paratranz.NET/Paratranz.NET.Test/ParatranzClientTest.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Paratranz.NET.Test
+{
+    [TestFixture]
+    public class ParatranzClientTest
+    {
+        [Test]
+        public void AuthorizationTest()
+        {
+            var apiToken = Environment.GetEnvironmentVariable("API_TOKEN");
+            Assert.IsNotNull(apiToken);
+
+            try
+            {
+                new ParatranzClient(apiToken);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
