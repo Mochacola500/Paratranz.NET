bbc76997cd8a22650f5d35fb4d91528107c004c6
Mochacola <techboydonghyeon@gmail.com>
2024-08-26 21:14:03 +0900

    P-8 ProjectSchema.cs를 CommonSchema.cs로 분리 - ProjectSchema에 공용 페이지에 사용할 S2C_PageResponse<TSchema> 클래스가 포함되어 있어 관리를 위해 분리

---
+++ Paratranz.NET/Paratranz.NET/Schema/CommonSchema.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Paratranz.NET
+{
+    public class S2C_PageResponse<TSchema>
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int RowCount { get; set; }
+        public int PageCount { get; set; }
+        public TSchema?[]? Results { get; set; }
+    }
+}
