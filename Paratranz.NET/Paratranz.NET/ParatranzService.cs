cffdec4feef8e5e0f9e422ad85c7abd4066f1cbb
Mochacola <techboydonghyeon@gmail.com>
2024-08-26 18:09:42 +0900

    P-6 ParatranzService 구현 - ParatranzClient를 활용하여 Paratranz 웹 서비스와 통신 하도록 구현 - 쿼리 파라미터를 만들기 위한 헬퍼 함수 추가

---
+++ Paratranz.NET/Paratranz.NET/ParatranzService.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+namespace Paratranz.NET
+{
+    public partial class ParatranzService : IDisposable
+    {
+        readonly ParatranzClient Client;
+        bool Disposed;
+
+        public ParatranzService(string apiToken)
+            : this(new ParatranzClient(apiToken))
+        {
+            
+        }
+
+        public ParatranzService(string apiToken, HttpClientHandler clientHandler)
+            : this(new ParatranzClient(apiToken, clientHandler))
+        {
+
+        }
+
+        public ParatranzService(string apiToken, HttpClientHandler clientHandler, bool disposeHandler)
+            : this(new ParatranzClient(apiToken, clientHandler, disposeHandler))
+        {
+            
+        }
+
+        public ParatranzService(ParatranzClient client)
+        {
+            Client = client;
+        }
+
+        #region IDisposable Implement
+
+        ~ParatranzService()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && !Disposed)
+            {
+                Disposed = true;
+                Client.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion // IDisposable Implement
+
+        #region Helper Implement
+
+        public static string ToQueryParams(params object[] queryParams)
+        {
+            if (queryParams.Length % 2 != 0)
+            {
+                throw new ArgumentException("Query parameters must be in key-value pairs.");
+            }
+
+            var queryString = new StringBuilder("?");
+            for (int i = 0; i < queryParams.Length; i += 2)
+            {
+                if (i > 0)
+                {
+                    queryString.Append("&");
+                }
+
+                string key = Uri.EscapeDataString(queryParams[i].ToString() ?? string.Empty);
+                string value = Uri.EscapeDataString(queryParams[i + 1].ToString() ?? string.Empty);
+                queryString.Append($"{key}={value}");
+            }
+
+            return queryString.ToString();
+        }
+
+        #endregion // Helper Implements
+    }
+}
