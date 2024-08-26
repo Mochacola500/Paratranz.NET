159e83f10645c190dade4877c3f82588b5b9f7f3
Mochacola <techboydonghyeon@gmail.com>
2024-08-26 18:11:20 +0900

    P-7 Project 구현 - Project Schema 추가 - Project NUnit 테스트 구현 - Project 서비스 통신 구현

---
+++ Paratranz.NET/Paratranz.NET/Schema/ProjectSchema.cs
@@ -0,0 +1,105 @@
+﻿namespace Paratranz.NET
+{
+    public class S2C_PageResponse<TSchema>
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int RowCount { get; set; }
+        public int PageCount { get; set; }
+        public TSchema?[]? Results { get; set; }
+    }
+
+    public class S2C_ProjectResponse : Data_Project
+    {
+
+    }
+
+    public class C2S_ProjectRequest
+    {
+        public string? Name { get; set; }
+        public Uri? Logo { get; set; }
+        public string? Desc { get; set; }
+        public string? Source { get; set; }
+        public string? Dest { get; set; }
+        public string? Game { get; set; }
+        public int Privacy { get; set; }
+        public int Download { get; set; }
+        public int IssueMode { get; set; }
+        public int ReviewMode { get; set; }
+        public int JoinMode { get; set; }
+    }
+
+    public class Data_Project
+    {
+        #region Field Declaration
+
+        public class FormatInfo
+        {
+            public string? yml { get; set; }
+            public string? txt { get; set; }
+        }
+
+        public class ExtraInfo
+        {
+            public string? Link { get; set; }
+            public bool Chars { get; set; }
+            public bool IsMod { get; set; }
+            public string? Credit { get; set; }
+            public string[]? Titles { get; set; }
+            public string? Version { get; set; }
+            public string? Compatible { get; set; }
+            public string? CreditLink { get; set; }
+            public bool CustomTests { get; set; }
+            public string? PublishLink { get; set; }
+            public bool HasTranslation { get; set; }
+            public bool DisableBatchSave { get; set; }
+        }
+
+        public class StatInfo
+        {
+            public int Id { get; set; }
+            public DateTime? DeletedAt { get; set; }
+            public DateTime? ModifiedAt { get; set; }
+            public int Total { get; set; }
+            public int Translated { get; set; }
+            public int Disputed { get; set; }
+            public int Checkd { get; set; }
+            public int Reviewed { get; set; }
+            public int Hidden { get; set; }
+            public int Locked { get; set; }
+            public int Words { get; set; }
+            public int Members { get; set; }
+            public float tp { get; set; }
+            public float cp { get; set; }
+            public float rp { get; set; }
+        }
+
+        #endregion // Field
+
+        public int Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int uid { get; set; }
+        public string? Name { get; set; }
+        public string? Logo { get; set; }
+        public string? Desc { get; set; }
+        public string? Source { get; set; }
+        public string? Dest { get; set; }
+        public int Members { get; set; }
+        public string? Game { get; set; }
+        public string? License { get; set; }
+        public float ActiveLevel { get; set; }
+        public int Stage { get; set; }
+        public int Privacy { get; set; }
+        public int Download { get; set; }
+        public int IssueMode { get; set; }
+        public int ReviewMode { get; set; }
+        public int JoinMode { get; set; }
+        public ExtraInfo? Extra { get; set; }
+        public StatInfo? Stats { get; set; }
+        public string[]? RelatedGames { get; set; }
+        public bool IsPrivate { get; set; }
+        public string? GameName { get; set; }
+        public FormatInfo? Formats { get; set; }
+    }
+}
---
+++ Paratranz.NET/Paratranz.NET/Service/ParatranzService.Project.cs
@@ -0,0 +1,36 @@
+﻿
+using Paratranz.NET.Schema;
+
+namespace Paratranz.NET
+{
+    public partial class ParatranzService
+    {
+        public Task<S2C_ProjectResponse?> CreateProjectAsync(C2S_ProjectRequest projectRequest, CancellationToken token)
+        {
+            string endPoint = "projects/";
+
+            return Client.PostAsync<C2S_ProjectRequest, S2C_ProjectResponse>(endPoint, projectRequest, token);
+        }
+
+        public Task<bool> DeleteProjectAsync(int projectId, CancellationToken token)
+        {
+            var endPoint = "projects/" + projectId;
+
+            return Client.DeleteAsync(endPoint, token);
+        }
+
+        public Task<S2C_ProjectResponse?> UpdateProjectAsync(int projectId, C2S_ProjectRequest projectRequest, CancellationToken token)
+        {
+            string endPoint = "projects/" + projectId;
+
+            return Client.PutAsync<C2S_ProjectRequest, S2C_ProjectResponse>(endPoint, projectRequest, token);
+        }
+
+        public Task<S2C_ProjectResponse?> GetProjectAsync(int projectId, CancellationToken token)
+        {
+            string endPoint = "projects/" + projectId;
+
+            return Client.GetAsync<S2C_ProjectResponse>(endPoint, token);
+        }
+    }
+}
